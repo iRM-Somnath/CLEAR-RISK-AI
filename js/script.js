36ca55b26f5b4caa29f7ddf4ec1b274b6ca74752
aritra <aritra.samanta@techriskpartners.com>
2024-10-14 15:45:58 +0530

    first commt

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const productImg = document.getElementById("#product-img");
+// const productButton1 = document.getElementById("product-btn-1");
+// const productButton2 = document.getElementById("product-btn-2");
+
+// productButton1.addEventListener("onclick", function() {
+//     productImg.src = "../assets/Component 1.png";
+// });
+
+// productButton2.addEventListener("onclick", function() {
+//     productImg.src = "../assets/Frame 12.png";
+// });
+
+function productButton1(){
+    productImg.src= "../assets/InternalAuditBanner_Web.png" ;
+    function productButton2(){
+
+        productImg.src= "../assets/EnterpriseRiskManagementBanner_Web.png" ;
+    }
+}
+
+
