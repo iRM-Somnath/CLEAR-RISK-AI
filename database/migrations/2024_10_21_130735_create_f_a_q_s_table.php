1ee6743c94f8a87c506abdf4dd6c591fa29a6b32
aritra <aritra.samanta@techriskpartners.com>
2024-10-30 17:26:34 +0530

    adding faq in cms

--- database/migrations/2024_10_21_130735_create_f_a_q_s_table.php
+++ database/migrations/2024_10_21_130735_create_f_a_q_s_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->text('question')->nullable();
             $table->text('answer')->nullable();
             $table->enum('status', [0, 1, 3])->default(1)->comment('0 = inactive, 1 = active, 3 = deleted');
-            $table->integer('created_by');
-            $table->integer('updated_by');
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
             $table->timestamps();
         });
     }
---
+++ resources/views/admin/pages/faqs-management/list.blade.php
@@ -0,0 +1,59 @@
+@extends('admin.layouts.master')
+@section('content')
+
+
+
+<section class="panel">
+    <header class="panel-heading">
+        <div class="panel-actions">
+            {{-- <a href="#" class="panel-action panel-action-toggle" data-panel-toggle></a>
+            <a href="#" class="panel-action panel-action-dismiss" data-panel-dismiss></a> --}}
+        </div>
+
+        {{-- <h2 class="panel-title">Ajax</h2> --}}
+    </header>
+    <div class="panel-body">
+        <table class="table table-bordered table-striped" >
+            <thead>
+                <tr>
+                    <th width="5%">Sl No.</th>
+                    <th width="40%">Quwstion</th>
+                    <th width="45%">Answer</th>
+                    {{-- <th width="30%">Plan Name</th> --}}
+                    {{-- <th width="5%">Currency</th> --}}
+                    {{-- <th width="10%">Price</th> --}}
+                    <th width="5%">Status</th>
+                    <th width="5%">Action</th>
+
+                </tr>
+                
+            </thead>
+            <tbody>
+                @forelse ($data as $item)
+                    <tr>
+                        <td>{{$loop->index + 1}}</td>
+                        <td>{{$item->question}}</td>
+                        <td>{{$item->answer}}</td>
+                        {{-- <td>{{$item->plan_name}}</td> --}}
+                        {{-- <td>{{$item->currency}}</td> --}}
+                        {{-- <td>{{$item->price}}</td> --}}
+                        <td>
+                            @if($item->status == 1)
+                            <span class="label label-success">Active</span>
+                            @else
+                            <span class="label label-danger">Inactive</span>
+                            @endif
+                        </td>
+                        <td>
+                            {{-- <a href="{{route('admin.question.edit', $item->id)}}" class="btn btn-sm btn-primary"><i class="fa fa-pencil"></i> Edit</a> --}}
+                            {{-- <form action="{{route('admin.question.destroy', $item->id)}}" method="POST" onsubmit="return confirm('Are you sure --}}
+                    </tr>
+                @empty
+                    
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+</section>
+
+@endsection
