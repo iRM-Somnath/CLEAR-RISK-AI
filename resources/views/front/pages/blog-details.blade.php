215dccbcf8f20159a02d6dfca82b3047c6405149
aritra <aritra.samanta@techriskpartners.com>
2024-11-12 17:28:55 +0530

    change in css

--- resources/views/front/pages/blog-details.blade.php
+++ resources/views/front/pages/blog-details.blade.php
@@ -1,12 +1,12 @@
 @extends('front.layouts.master')
 @section('content')
     <div class="blog-section px-5 d-flex flex-column gap-20">
-        <div class="blog-text-img col-xl-8 col-lg-8 col-md-8 col-sm-12 gap-20 mx-auto">
+        <div class="blog-text-img col-xl-8 col-lg-8 col-md-8 col-sm-12 flex-col gap-20 mx-auto">
             <h1 class="display-5 fw-bold lh-1 mb-3 main-heading-dark">
                 {{ $blog->title ?? '' }}
             </h1>
-            <div class="findout-img d-flex">
-                <img src="{{  !is_null($blog)?asset('uploads/blog/'.$blog->image ):"" }}" class="m-3" alt="ClearRiskGif"
+            <div class="blog-detail-img d-flex">
+                <img src="{{  !is_null($blog)?asset('uploads/blog/'.$blog->image ):"" }}" class="" alt="ClearRiskGif"
                     width="700" height="500" loading="lazy" />
             </div>
 
