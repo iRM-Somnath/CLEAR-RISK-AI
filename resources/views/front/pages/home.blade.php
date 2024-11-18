55788982195a089ce3eaca2fdc761583bcd6c062
aritra <aritra.samanta@techriskpartners.com>
2024-11-18 15:29:07 +0530

    Redesign the contact us from

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -164,7 +164,7 @@
     </div>
 
 
-    <div class="product-container margin50 d-flex flex-column align-items-center gap-20">
+    <div class="product-container d-flex flex-column align-items-center gap-20">
         <div class="col-10 col-sm-8 col-lg-6 w-100">
             <div class="heading d-flex justify-content-center">
                 <h1 class="display-5 fw-bold lh-1 mb-3 main-heading-dark">
